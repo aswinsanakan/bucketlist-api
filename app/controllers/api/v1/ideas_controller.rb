2cf81e206eb8bc71d5528aafceb0f0ef7fea239d
aswinsanakan <aswinsanakan@gmail.com>
2017-10-12 16:19:13 +0530

    Add create and update actions for Ideas

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,8 +1,25 @@
 module Api::V1
   class IdeasController < ApplicationController
     def index
-      @ideas = Idea.all.order("updated_at desc")
+      @ideas = Idea.all.order("created_at desc")
       render json: @ideas
+    end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    def update
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
+      render json: @idea
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
     end
   end
 end
